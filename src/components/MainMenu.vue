0213d56f40b8871a4eb43443b974a1b24b2dff65
Stephane Doiron <didymus1313@gmail.com>
2018-05-28 20:58:44 -0400

    menu active fix

--- src/components/MainMenu.vue
+++ src/components/MainMenu.vue
@@ -2,13 +2,13 @@
   <nav class="navbar">
     <div class="navbar-brand">
       <div class="navbar-item">Apps!</div>
-      <div class="navbar-burger" data-target="navMenu">
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
+      <a role="button" class="navbar-burger" :class="{'is-active': isActive}" @click="isActive = !isActive">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
     </div>
-    <div class="navbar-menu">
+    <div class="navbar-menu" :class="{'is-active': isActive}">
       <div class="navbar-start">
         <router-link :to="{ name: 'math', params: {} }" class="navbar-item">Math for Kids!</router-link>
         <router-link :to="{ name: 'encounter-calculator', params: {} }" class="navbar-item">Encounter Calculator</router-link>
@@ -27,27 +27,11 @@ import LocaleSwitcher from './LocaleSwitcher'
 export default {
   components: {
     LocaleSwitcher
+  },
+  data () {
+    return {
+      isActive: false
+    }
   }
 }
-
-document.addEventListener('DOMContentLoaded', function () {
-  // Get all "navbar-burger" elements
-  var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0)
-
-  // Check if there are any navbar burgers
-  if ($navbarBurgers.length > 0) {
-    // Add a click event on each of them
-    $navbarBurgers.forEach(function ($el) {
-      $el.addEventListener('click', function () {
-        // Get the target from the "data-target" attribute
-        var target = $el.dataset.target
-        var $target = document.getElementById(target)
-
-        // Toggle the class on both the "navbar-burger" and the "navbar-menu"
-        $el.classList.toggle('is-active')
-        $target.classList.toggle('is-active')
-      })
-    })
-  }
-})
 </script>
